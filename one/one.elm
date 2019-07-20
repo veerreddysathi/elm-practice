ab9fb34d36f119b6bc2609b9f2d201eb03fa1100
veerreddy <veerreddy_sathi@yahoo.in>
2019-07-21 00:34:16 +0200

    Increment and Decrement Counter

--- one/one.elm
+++ one/one.elm
@@ -1,5 +1,57 @@
 module Main exposing (..)
 
-import Html
+import Html exposing (..)
+import Html.Events exposing (..)
 
-main = Html.text "Hello World!"
+
+main =
+    Html.beginnerProgram
+        { model = model
+        , view = view
+        , update = update
+        }
+
+
+
+--model
+
+
+type alias Model =
+    Int
+
+
+model : Model
+model =
+    0
+
+
+
+--update
+
+
+type Msg
+    = Inc
+    | Dec
+
+
+update : Msg -> Model -> Model
+update msg model =
+    case msg of
+        Inc ->
+            model + 1
+
+        Dec ->
+            model - 1
+
+
+
+--view
+
+
+view : Model -> Html Msg
+view model =
+    div []
+        [ button [ onClick Dec ] [ text "-" ]
+        , div [] [ text (toString model) ]
+        , button [ onClick Inc ] [ text "+" ]
+        ]
